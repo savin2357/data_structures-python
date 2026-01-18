08658e481bed1aa64bdaa84a567f75977fbef286
savjr <savindujr@gmail.com>
2026-01-18 15:59:46 +1300

    loops and functions

--- 5-loops.py
+++ 5-loops.py
@@ -1,9 +1,14 @@
-def power_func(num):
+def power_func0(num):
     for i in range(num):
         print(i*i)
+
+def power_func1(num):
+    i=1
+    while i<=num:
+        print(i*i)
+        i=i+1
         
-
-
 if __name__ == '__main__':
     n = int(input())
-    power_func(n)
+    power_func1(n)
+
